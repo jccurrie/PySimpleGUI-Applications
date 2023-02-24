2ac460c38c15cb3d298f315aa550783ca4e35da4
Julian Currie <jccurrie@uab.edu>
2023-02-23 19:11:49 -0600

    finished converter app

--- Conversion Application/converter_application.py
+++ Conversion Application/converter_application.py
@@ -7,12 +7,15 @@ import PySimpleGUI as sg
 # Layout is a list (main window) of lists,
 # each list a row (section of main window)
 # optional arguments in the sublists create new columns in that row
+# window['-TEXT1-'].update(values['-INPUT-']) (changes value of key)
+# window['-TEXT1-'].update(visible = False) (hides the element)
+
 layout = [
-    [sg.Text('Text'), sg.Spin(['item 1', 'item 2', 'item 3'])],
+    [sg.Text('Lbs. to Oz.', enable_events = True, key = '-TEXT1-'), \
+            sg.Spin(['item 1', 'item 2', 'item 3'], key = '-LBSTOOZ-')],
     # Button takes (Button text, Key for an ID)
-    [sg.Button('Button', key = 'Button1')],
-    [sg.Input()],
-    [sg.Text('Hello'), sg.Button('Test Button', key = 'Button2')]
+    [sg.Text('Input your value: '), sg.Input(key = '-INPUT-')],
+    [sg.Button('Convert', key = '-CONVERTBTN-'), sg.Text('0', key = '-OUTPUT-'),],
 ]
 
 window = sg.Window('Conversion Application', layout)
@@ -23,10 +26,12 @@ while True:
     if event == sg.WINDOW_CLOSED:
         break
 
-    if event == 'Button1':
-        print('Button Pressed!')
+    if event == '-CONVERTBTN-':
+        value = int(values['-INPUT-'])
+        window['-OUTPUT-'].update(str(value * 16))
 
-    if event == 'Button2':
-        print('The Test Button Pressed!')
+
+    if event == '-TEXT1-':
+        print('Text was pressed')
 
 window.close()
